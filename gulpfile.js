2d385cc98a93556c09e636e7e6ed702f181e7e1f
Alex Aloia <tripl3infinity@gmail.com>
2014-10-28 14:05:36 -0500

    adding svg sprite gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,79 @@
 var theme_name = 'roots';
 
-var gulp = require('gulp');
-//var mainBowerFiles = require('main-bower-files');
-var jshint = require('gulp-jshint');
-var watch = require('gulp-watch');
-var copy = require('gulp-copy');
-var less = require('gulp-less');
-var minifyCSS = require('gulp-minify-css');
-var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require("gulp-rename");
-var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
+var gulp = require('gulp'),
+//var mainBowerFiles = require('main-bower-files'),
+    jshint = require('gulp-jshint'),
+    watch = require('gulp-watch'),
+    copy = require('gulp-copy'),
+    less = require('gulp-less'),
+    minifyCSS = require('gulp-minify-css'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require("gulp-rename"),
+    browserSync = require('browser-sync'),
+    plumber = require('gulp-plumber'),
+    phantomjs = require('phantomjs'),
+    svgSprite = require("gulp-svg-sprites");
 
+    reload = browserSync.reload;
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync({
-        proxy: "dev.t3inf.com"
+        proxy: "dev.t3inf.com",
+        ghostMode: true
     });
 });
+
+
+/*
+ * svg min & sprite
+ */
+gulp.task('sprites', function () {
+    return gulp.src('src/svg/*.svg')
+        .pipe(plumber())
+        .pipe(svgSprite({
+            mode: "symbols",
+            svg: {
+                symbols: "inline-symbols.svg.php"
+            }
+        }))
+        .pipe(gulp.dest('./themes/' + theme_name + '/assets/svg'));
+});
+
 
 /*
  * copy src images
  */
-gulp.task('img-copy', function() {
-  gulp.src('src/svg/*.svg')
- .pipe(gulp.dest('./themes/' + theme_name + '/assets/svg'));
+gulp.task('img-copy', function () {
+    gulp.src('src/svg/*.svg')
+        .pipe(gulp.dest('./themes/' + theme_name + '/assets'));
 });
-
 
 
 /*
  * Less / CSS
  */
 gulp.task('less_dev', function () {
-  gulp.src('src/less/main.less')
-    .pipe(sourcemaps.init())
-    .pipe(less())
-    .pipe(minifyCSS({keepBreaks:true, debug:true}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./themes/' + theme_name + '/assets/css'));
+    gulp.src('src/less/main.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(minifyCSS({keepBreaks: true, debug: true}))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./themes/' + theme_name + '/assets/css'));
 });
 
 gulp.task('less', function () {
-  gulp.src('src/less/main.less')
-    .pipe(less())
-    .pipe(minifyCSS({keepBreaks:false, debug:false}))
-    .pipe(rename('main.min.css'))
-    .pipe(gulp.dest('./themes/' + theme_name + '/assets/css'));
+    gulp.src('src/less/main.less')
+        .pipe(less())
+        .pipe(minifyCSS({keepBreaks: false, debug: false}))
+        .pipe(rename('main.min.css'))
+        .pipe(gulp.dest('./themes/' + theme_name + '/assets/css'));
 });
 
 
-
 /*
-* javascript
+ * javascript
  */
 
 var js_head = [
@@ -82,14 +101,14 @@ var js_foot = [
     'src/js/_*.js'
 ];
 
-gulp.task('js-head', function() {
+gulp.task('js-head', function () {
     gulp.src(js_head)
         .pipe(concat('js-head.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('themes/' + theme_name + '/assets/js'))
 });
 
-gulp.task('js-foot', function() {
+gulp.task('js-foot', function () {
     gulp.src(js_head)
         .pipe(concat('js-foot.min.js'))
         .pipe(uglify())
@@ -97,7 +116,7 @@ gulp.task('js-foot', function() {
 });
 
 
-gulp.task('dev_js-head', function() {
+gulp.task('dev_js-head', function () {
     gulp.src(js_head)
         .pipe(sourcemaps.init())
         .pipe(concat('js-head.js'))
@@ -106,7 +125,7 @@ gulp.task('dev_js-head', function() {
 });
 
 
-gulp.task('dev_js-foot', function() {
+gulp.task('dev_js-foot', function () {
     gulp.src(js_foot)
         .pipe(sourcemaps.init())
         .pipe(concat('js-foot.js'))
@@ -122,19 +141,19 @@ gulp.task('jshint', function () {
 });
 
 
-gulp.task('bower', function() {
-    return gulp.src(mainBowerFiles(/* options */), { base: 'vendor/bower' })
+gulp.task('bower', function () {
+    return gulp.src(mainBowerFiles(/* options */), {base: 'vendor/bower'})
         .pipe(concat('main.min.js'))
         .pipe(jshint())
         .pipe(gulp.dest('themes/' + theme_name + '/assets/js'))
 });
 
 /*
-* TASK LIST
-*/
+ * TASK LIST
+ */
 
 // Dev
-gulp.task('watch', ['browser-sync'], function() {
+gulp.task('watch', ['browser-sync'], function () {
     // Watch PHP files
     gulp.watch('themes/' + theme_name + '/**/*.php', browserSync.reload);
     // Watch less files
@@ -146,7 +165,7 @@ gulp.task('watch', ['browser-sync'], function() {
 });
 
 // Default task to be run with `gulp`
-gulp.task('dev', ['img-copy','less_dev', 'dev_js-foot', 'jshint', 'watch', 'browser-sync']);
+gulp.task('dev', ['img-copy', 'less_dev', 'dev_js-foot', 'jshint', 'watch', 'browser-sync']);
 
 // Build
-gulp.task('default', ['less','js-head', 'js-foot']);
+gulp.task('default', ['less', 'js-head', 'js-foot']);
