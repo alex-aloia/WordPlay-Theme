50ec69286d2f14cbdec27790b1416c45b196cae4
Alex Aloia <tripl3infinity@gmail.com>
2015-02-24 14:40:43 -0600

    trying dif google font approach

--- themes/custom/templates/head.php
+++ themes/custom/templates/head.php
@@ -6,6 +6,21 @@
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width">
 
+  <script type="text/javascript">
+    WebFontConfig = {
+      google: { families: [ 'Orbitron::latin' ] }
+    };
+    (function() {
+      var wf = document.createElement('script');
+      wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+      wf.type = 'text/javascript';
+      wf.async = 'true';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(wf, s);
+    })(); </script>
+
+
   <?php
       //get_template_part('assets/img/inline', 'symbols.svg');
       wp_head();
@@ -13,6 +28,5 @@
 
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
-
-
+  
 </head>
